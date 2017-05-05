ed84d7b1f41886844b5e5780d13a3d7cb5a3115c
Florent Vion <florent.vion@st.com>
2017-05-05 10:19:15 +0200

    - refactoring the repository     - move template com.vionf.preferences.pages in preferences folder - add template menu/com.vionf.submenu

---
+++ preferences/com.vionf.templates/src/com/vionf/preferences/pages/pages/MyPropertyChangeListener.java
@@ -0,0 +1,50 @@
+/*******************************************************************************
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Florent Vion
+ *******************************************************************************/
+package com.vionf.templates.preferences.pages;
+
+import org.eclipse.jface.preference.FieldEditor;
+import org.eclipse.jface.preference.StringButtonFieldEditor;
+import org.eclipse.jface.util.IPropertyChangeListener;
+import org.eclipse.jface.util.PropertyChangeEvent;
+
+/**
+ * @author vionf
+ *
+ */
+public class MyPropertyChangeListener implements IPropertyChangeListener {
+	
+	private MyPreferencePage page;
+	
+	/**
+	 * Constructor
+	 * @param nodePreferencePage
+	 */
+	public MyPropertyChangeListener(MyPreferencePage nodePreferencePage) {
+		this.page = nodePreferencePage;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.jface.util.IPropertyChangeListener#propertyChange(org.eclipse.jface.util.PropertyChangeEvent)
+	 */
+	@Override
+	public void propertyChange(PropertyChangeEvent event) {
+		page.propertyChange(event);
+		
+		// The IS_VALID property is only fired when the valid state changes.
+		if (event.getProperty().equals(FieldEditor.IS_VALID)) {
+			Object source = event.getSource();
+			if (source instanceof StringButtonFieldEditor) {
+				StringButtonFieldEditor strButFieldEditor = (StringButtonFieldEditor) source;
+				String stringValue = strButFieldEditor.getStringValue();
+				page.getLabelFieldEditor().setLabelText(stringValue);
+			}
+		}
+	}
+}
